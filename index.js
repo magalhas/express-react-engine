fc66770a8eaa494b1c7b9baa205871c0342bd985
José Magalhães <magalhas@gmail.com>
2015-02-17 12:48:11 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+'use strict';
+
+var React = require('react');
+var nodeJSX = require('node-jsx');
+var merge = require('lodash.merge');
+
+module.exports = function engineFactory (engineOptions) {
+  engineOptions = merge(engineOptions || {}, {
+    jsx: {
+      extension: '.jsx',
+      harmony: false
+    },
+    docType: '<!DOCTYPE html>',
+    staticMarkup: false
+  });
+
+  nodeJSX.install(engineOptions.jsx);
+
+  return function renderComponent (filename, options, callback) {
+    options = options || {};
+
+    try {
+      var markup = engineOptions.docType;
+
+      var Component = require(filename);
+
+      var instance = React.createElement(Component, options.props);
+
+      if (engineOptions.staticMarkup) {
+        markup += React.renderToStaticMarkup(instance);
+      } else {
+        markup += React.renderToString(instance);
+      }
+
+      callback(null, markup);
+    } catch (error) {
+      callback(error);
+    }
+  };
+};
